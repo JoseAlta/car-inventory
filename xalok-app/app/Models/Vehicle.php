01ae9a750fb4fcdd187ee421f77351b89fc6d198
josé Altamirano <jose@retrypay.com>
2024-04-08 02:17:15 -0500

    add model and migration files

---
+++ xalok-app/app/Models/Vehicle.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Vehicle extends Model
+{
+    use HasFactory;
+
+    protected $perPage = 20;
+
+    /**
+     * Attributes that should be mass-assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['brand', 'model', 'plate', 'licenseRequired'];
+
+
+}
